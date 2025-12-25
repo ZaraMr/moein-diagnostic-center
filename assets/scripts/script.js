f7925c4865ea620a9aa6c290cfc3626f90e60aeb
ZaraMr <zhra.mrdi2907@gmail.com>
2025-12-25 16:55:04 +0330

    Refien responsive styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,22 @@
 "use strict";
 
+// variables
 const menuBtn = document.querySelector(".menu-btn");
 const menuExitBtn = document.querySelector(".menu-exit");
 const mobileNav = document.querySelector(".nav-list");
 const body = document.body;
 
+// open mobile menu when menu icon clicked
 menuBtn.addEventListener("click", () => {
   body.classList.toggle("show-mobile-menu");
 });
 
+// close mobile menu when exit icon clicked
 menuExitBtn.addEventListener("click", () => {
-  body.classList.toggle("show-mobile-menu");
+  body.classList.remove("show-mobile-menu");
 });
 
+// close mobile menu when outside of menu clicked
 function outsideClick(e) {
   if (!body.classList.contains("show-mobile-menu")) return;
   if (e.target.closest(".nav-list") || e.target.closest(".menu-btn")) return;
