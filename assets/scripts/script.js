d78308c0a6b1b4f741bf9db80c73c20453200abd
ZaraMr <zhra.mrdi2907@gmail.com>
2025-12-29 01:40:55 +0330

    dd intersect stection styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,29 @@ const menuBtn = document.querySelector(".menu-btn");
 const menuExitBtn = document.querySelector(".menu-exit");
 const mobileNav = document.querySelector(".nav-list");
 const body = document.body;
+const sections = document.querySelectorAll(".section");
+console.log(sections);
+
+// reveal sections
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+sections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
 
 // open mobile menu when menu icon clicked
 menuBtn.addEventListener("click", () => {
